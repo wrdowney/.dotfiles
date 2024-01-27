d2764d8ac9dc4b2bc275b3092ac7a791d70b9774
wrdowney <willd20@vt.edu>
2024-01-27 15:17:50 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt install python3-launchpadlib
+
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git neovim stow -y
+
+git config --global init.defaultBranch main
+git config --global user.name "wrdowney"
+git config --global user.email "willd20@vt.edu"
